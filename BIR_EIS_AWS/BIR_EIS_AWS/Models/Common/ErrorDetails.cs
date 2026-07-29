d4b841cec34d38556f2816fc7c9d94ba41d9ae77
Charlemagne J. Gustilo <98066153+cjg2243@users.noreply.github.com>
2026-07-29 10:49:29 +0800

    Delete BIR_EIS_AWS/BIR_EIS_AWS directory

--- BIR_EIS_AWS/BIR_EIS_AWS/Models/Common/ErrorDetails.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BIR_EIS_AWS.Models.Common
-{
-    public class ErrorDetails
-    {
-        public string errorCode { get; set; }
-
-        public string errorMessage { get; set; }
-    }
-}
